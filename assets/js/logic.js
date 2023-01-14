d1168753c1122acd202692ccb0c5cd59470d6863
georgeb <george.boholteanu@gmail.com>
2023-01-14 17:21:49 +0000

    logic.js pseudo code

---
+++ assets/js/logic.js
@@ -0,0 +1,27 @@
+// set the timer variable
+// define the questions and answers
+
+
+
+// on button start clicked 
+    //  run a loop through all the questions -- while timer !==0
+        // start timer
+        // create a container with the question and answers
+        // create a button for each answer
+        // for each button answer have an action and score assigned to it
+        // if correct answer is picked 
+                // create a new span confirming result 
+                // store result to localStorage
+                // go to next question
+        // else 
+            // create a new span confirming result
+            //  reducing 10 from timer variable 
+            //  store result to localStorage
+            //  go to next question
+        
+        // all done introduce you initials in to a field
+        // store initials to localStorage
+
+    // else 
+        // quizz ends showing a message
+        // create the html of the high score with JS from LocalStorage
