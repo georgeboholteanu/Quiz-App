17ea34392c612fe73f1edaabaabe6d28e2a362c4
georgeb <george.boholteanu@gmail.com>
2023-01-14 18:53:05 +0000

    added buttons with click event to answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,32 +4,36 @@ var startScr = document.getElementById("start-screen");
 var questions = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
 var choices = document.getElementById("choices");
+var reply = document.createElement("p");
+var replyIt = document.createElement("i");
+var hr = document.createElement("hr");
+replyIt.style.color = "grey";
 
 var questionare = {
     item1: {
         question: "Commonly used data types DO NOT include:",
-        answer: ["strings", "booleans", "alerts", "numbers"],
-        solution: "alerts"
+        answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        solution: "3. alerts"
     },
     item2:{
         question: "The condition in an if / else statement is enclosed within _____.",
-        answer: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        solution: "parentheses"
+        answer: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        solution: "3. parentheses"
     },
     item3:{
         question: "Arrays in JavaScript can be used to store _____.",
-        answer: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        solution: "all of the above"
+        answer: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        solution: "4. all of the above"
     },
     item4:{
         question: "String values must be enclosed within ____ when being assigned to variables.",
-        answer: ["commas", "curly brackets", "quotes", "parentheses"],
-        solution: "quotes"
+        answer: ["1. commas", "1. curly brackets", "3. quotes", "4. parentheses"],
+        solution: "3. quotes"
     },
     item5:{
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        solution: "console.log"
+        answer: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        solution: "4. console.log"
     }
 }
 
@@ -41,22 +45,32 @@ startBtn.addEventListener("click", function() {
     questions.style.display = "block";
 
     // create questions dinamicaly 
+    // var items = Object.keys(questionare);   
 
-    questionTitle.textContent = questionare.item1.question;
+        questionTitle.textContent = questionare.item1.question;
 
-    var answerList = document.createElement("ol");
-    questions.appendChild(answerList);
+        questionare.item1.answer.forEach(element => {
+            var btn = document.createElement("button");           
+            btn.textContent = element;
+            choices.appendChild(btn);
 
-    questionare.item1.answer.forEach(element => {
-        var liEl = document.createElement("li");
-        var liBtn = document.createElement("button");
-        liBtn.textContent = element;
-        answerList.appendChild(liEl);
-        liEl.appendChild(liBtn);
+            btn.addEventListener("click", function() {
+                //show result
+                if (element === questionare.item1.solution) { 
+                    choices.appendChild(hr)    
+                    reply.appendChild(replyIt); 
+                    choices.appendChild(reply);               
+                    replyIt.textContent = "Correct!";                               
+                }else {
+                    choices.appendChild(hr)
+                    reply.appendChild(replyIt);
+                    choices.appendChild(reply);  
+                    replyIt.textContent = "Wrong!";
+                }
+            });          
+       
     });
-    
-    console.log(questionTitle.innerHTML);    
-    
+
 });
 
 
