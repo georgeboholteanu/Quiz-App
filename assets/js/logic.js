b94984c5bf01d450110beabfdc5b81f969f1bc3e
georgeb <george.boholteanu@gmail.com>
2023-01-14 22:54:04 +0000

    added renderQuestion addClickListerner removeClickListerner and handleNext functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,36 +7,33 @@ var choices = document.getElementById("choices");
 var reply = document.createElement("p");
 var replyIt = document.createElement("i");
 var hr = document.createElement("hr");
+var endScr = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+
 replyIt.style.color = "grey";
 
-var questionare = {
-    item1: {
+var questionnaire = [{
         question: "Commonly used data types DO NOT include:",
-        answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         solution: "3. alerts"
-    },
-    item2:{
+    },{
         question: "The condition in an if / else statement is enclosed within _____.",
-        answer: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        options: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
         solution: "3. parentheses"
-    },
-    item3:{
+    },{
         question: "Arrays in JavaScript can be used to store _____.",
-        answer: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         solution: "4. all of the above"
-    },
-    item4:{
+    },{
         question: "String values must be enclosed within ____ when being assigned to variables.",
-        answer: ["1. commas", "1. curly brackets", "3. quotes", "4. parentheses"],
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
         solution: "3. quotes"
-    },
-    item5:{
+    },{
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
         solution: "4. console.log"
     }
-}
-
+]
 
 startBtn.addEventListener("click", function() {
 
@@ -44,34 +41,62 @@ startBtn.addEventListener("click", function() {
     startScr.style.display = "none";    
     questions.style.display = "block";
 
-    // create questions dinamicaly 
-    // var items = Object.keys(questionare);   
+    var score =  0;
+    var index = 0;    
 
-        questionTitle.textContent = questionare.item1.question;
+    const renderQuestion = () => {    
+        var item = questionnaire[index];      
 
-        questionare.item1.answer.forEach(element => {
-            var btn = document.createElement("button");           
-            btn.textContent = element;
-            choices.appendChild(btn);
+        score = localStorage.getItem("score");
 
-            btn.addEventListener("click", function() {
-                //show result
-                if (element === questionare.item1.solution) { 
-                    choices.appendChild(hr)    
-                    reply.appendChild(replyIt); 
-                    choices.appendChild(reply);               
-                    replyIt.textContent = "Correct!";                               
-                }else {
-                    choices.appendChild(hr)
-                    reply.appendChild(replyIt);
-                    choices.appendChild(reply);  
-                    replyIt.textContent = "Wrong!";
-                }
-            });          
-       
-    });
+        // Clean-up since we are removing the DOM
+        removeClickListener(questions.querySelector('.optBtn'), handleNext);
+        // removeClickListener(questions.querySelector('.optBtn'), handleSubmit);
+        
+        questions.innerHTML = `
+            <h2 id="question-title">${item.question}</h2>
+            <div id="choices" class="choices">
+            <button class="optBtn">${item.options[0]}</button>
+            <button class="optBtn">${item.options[1]}</button>
+            <button class="optBtn">${item.options[2]}</button>      
+            <button class="optBtn">${item.options[3]}</button>      
+            </div>
+        `;
+    
+        // Re-associate the listener
+        var newButtons = document.querySelectorAll('.optBtn');
+        newButtons.forEach((btn) => {    
+            addClickListener(btn, handleNext);              
+        })
 
-});
+    }
+    
+    const addClickListener = (btn, listener) => {
+        if (btn) btn.addEventListener('click', listener);
+      };
+    
+    const removeClickListener = (btn, listener) => {
+    if (btn) btn.removeEventListener('click', listener);
+    };
+    
+    const handleNext = e => {
+        e.preventDefault();        
+
+        if (index < questionnaire.length - 1) {
+          index++;
+          renderQuestion();
+        } else {
+          questions.style.display = 'none';
+          endScr.style.display = 'block';   
+          finalScore.textContent = score;       
+        }
+      };
+      
+    localStorage.setItem("score", score);
+    renderQuestion();
+}); // end of eventListener
+
+
 
 
 // set the timer variable
